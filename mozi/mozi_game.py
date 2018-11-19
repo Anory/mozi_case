45df76762c8d9350f0fbeefc811645c54f1782a3
yangguibing <876522068@qq.com>
2018-11-19 13:47:35 +0800

    测试墨子游戏

--- mozi/mozi_game.py
+++ mozi/mozi_game.py
@@ -1,6 +1,7 @@
 import time
 import pymysql
 import requests
+from websocket import create_connection
 import websocket
 import threading
 
@@ -16,7 +17,7 @@ mozi_connect = pymysql.Connect(
 # 获取游标
 mozi_cursor = mozi_connect.cursor()
 sql = "SELECT username FROM user_info_kaicai WHERE id BETWEEN 650 AND 2000"
-# sql = "SELECT username FROM user_info_kaicai WHERE username in('mkij', 'jies')"
+# sql = "SELECT username FROM user_info_kaicai WHERE username in('mkij')"
 mozi_cursor.execute(sql)
 name = mozi_cursor.fetchall()  # 获取查询出来的内容
 name_list = []
@@ -25,14 +26,12 @@ for i in name:
 
 
 class ThreadTest(threading.Thread):
-    def __init__(self, username, token_lists):
+    def __init__(self, username):
         threading.Thread.__init__(self)
         self.username = username
-        self.token_lists = token_lists
 
     def get_response(self):
         for account in self.username:
-            ws = websocket.WebSocket()
             data = {
                 "account": account,
                 "password": "123456"
@@ -42,37 +41,39 @@ class ThreadTest(threading.Thread):
             # print(res)
             token = res['data']['token']
             # print(token)
-            # time.sleep(1)
-            self.token_lists.insert(0, token)  # 将获取到的token存放到列表里面
-            # print(self.token_lists)
-            file = open('../data/token.txt', 'w')  # 将token_list文件存放到本地的txt文件中
-            file.write(str(self.token_lists))
-            file.close()
-            ws.connect("ws://10.0.3.31:16050/websocket/{0}/1000".format(token))
-            if ws.connected:
-                result = ws.recv()
-                print("链接返回的结果：", result)
-                # ws.close()
+            time.sleep(5)
+            url = 'ws://10.0.3.21:19090/ws?token={0}'
+            while True:  # 一直链接，直到连接上就退出循环
+                # time.sleep(0.5)
+                try:
+                    ws = create_connection(url.format(token))
+                    ws.send("{'i':1,'t':123456,'c':'joinRoom','d':{'detailId':1000}}")
+                    response = ws.recv()
+                    print(response)
+                    # print(ws)
+                    break
+                except Exception as e:
+                    print('连接异常：', e)
+                    continue
+            # while True:  # 连接上，退出第一个循环之后，此循环用于一直获取数据
+            #     # time.sleep(0.5)
+            #     ws.send("{'i':1,'t':123456,'c':'joinRoom','d':{'detailId':1000}}")
+            #     response = ws.recv()
+            #     print(response)
+            #     break
 
 
 if __name__ == "__main__":
-    token_list = []
-    run = ThreadTest(name_list, token_list)
+    run = ThreadTest(name_list)
     run.get_response()
     t_list = []
-    count = 1
-    for i in range(count):
-        star_time = time.time()
-        print("-------------------------------------")
-        print("线程1开始时间：", star_time)
-        t1 = threading.Thread(target=run.get_response())
-        print("--------------线程分割--------------")
-        print("线程2开始时间：", star_time)
-        t2 = threading.Thread(target=run.get_response())
-        t_list.append(t1)
-        t_list.append(t2)
-        t1.start()
-        t2.start()
+    for i in range(10):
+        start_time = time.time()
+        print(start_time)
+        t = threading.Thread(target=run.get_response())
+        t_list.append(t)
+    for t in t_list:
+        t.start()
     for t in t_list:
         t.join()
 
